4133a386e5f283fd32b5f32a1f3ba4e8a29cdb61
Святослав Кураленок <svyatoslav.sk4@gmail.com>
2025-02-26 16:07:27 +0300

    Add: some READMEs

--- scripts/build_examples.sh
+++ scripts/build_examples.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 pwd="$(pwd | rev | cut -d '/' -f 1 | rev)"
-echo ${pwd}
 if [ "${pwd}" = "algorithms_analysis" ]; then
     cd examples
     mkdir build 2> /dev/null
     cd build
     cmake ..
     make -j 2> /dev/null
-    ./*_example
 else
-    echo "Please change directory to .../algorithms_analysis"
+    echo "Please cd to algorithms_analysis directory"
 fi
 
 # Usage: sh scripts/build_algo_analysis.sh
--- scripts/format_source_code.sh
+++ scripts/format_source_code.sh
@@ -2,10 +2,15 @@
 
 files="$(find . -regex ".*/*\.[c|h]pp" 2> /dev/null)"
 
-if [ $1 -gt 0 ]; then
-    echo "Found files to format: ${files}" 
+if [ -z "$1" ]
+then
+    echo ""
+else 
+    if [ $1 -gt 0 ]; then
+        echo "Found files to format: ${files}" 
+    fi
 fi
 
 echo ${files} | xargs clang-format -style=file -i 2>/dev/null
 
-# Usage: sh scripts/format_source_code.sh 0/1
+# Usage: sh scripts/format_source_code.sh
