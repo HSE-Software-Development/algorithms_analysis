1d3a9670236ef3884bcf65e701e612b84795fc7f
Dmitrii Artiukhov <artuhovdmitrii@gmail.com>
2025-03-03 01:45:02 +0000

    Update boost.cmake

--- cpp/cmake/boost.cmake
+++ cpp/cmake/boost.cmake
@@ -1,12 +1,4 @@
 
-# include(FetchContent)
-# FetchContent_Declare(
-#   boost
-#   GIT_REPOSITORY https://github.com/boostorg/boost.git
-#   GIT_TAG boost-1.83.0
-#   GIT_SHALLOW TRUE
-# )
-# FetchContent_MakeAvailable(boost)
 set(BOOST_INCLUDE_LIBRARIES heap)
 set(BOOST_ENABLE_CMAKE ON)
 include(FetchContent)
